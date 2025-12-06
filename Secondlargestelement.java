4995d2c8d77880a5a3c5d8c85bbb8db97e9d9e4d
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2025-12-06 20:37:12 +0530

    DSA

---
+++ Secondlargestelement.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+class Secondlatgestelement{
+    public static int secondlargest(int[] nums){
+        HashSet<Integer>set = new HashSet<>();
+        for(int i = 0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        int max = Collections.max(set);
+        set.remove(max);
+        max = Collections.max(set);
+        return max;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Entee the Number :");
+        int n = scan.nextInt();
+        System.out.println("Enter the Size :");
+        int[] nums = new int[n];
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = scan.nextInt();
+        }
+        int result = secondlargest(nums);
+        System.out.println("Second Largest Element :"+result);
+
+    }
+}
